8a78b5902586d09ee18acb22c936986928e34cc9
Sun4ru <85418918+Sun4ru@users.noreply.github.com>
2023-08-28 19:36:25 +0200

    Updated cube.js

--- cube.js
+++ cube.js
@@ -33,12 +33,10 @@ class Cube {
             let t12 = i/(div+1);
             let t13 = j/(div+1);  
             let point = new Vec4(v12.x*t12 + v13.x*t13 + p1.x, v12.y*t12 + v13.y*t13 + p1.y, v12.z*t12 + v13.z*t13 + p1.z, 1);
-            let center_to_point = new Vec4(point.x-center.x, point.y-center.y, point.z-center.z, 1);
-            let normalized_vec = center_to_point.normalizedVec();
-            this.FrontPoints.push(new Vec4(normalized_vec.x+center.x, normalized_vec.y+center.y, normalized_vec.z+center.z, 1));
-            //this.FrontPoints.push(point)
+            this.FrontPoints.push(point)
         }
         }
+        //this.ToSphere(this.FrontPoints);
 
         //goes through every point to form triangles
         //every point is linked to 2 triangles
@@ -50,6 +48,7 @@ class Cube {
         }
         }
 
+        //
         //LEFT (P1,P2,P5,P6)
         this.LEFTPoints = [];
         this.LEFTTris = [];
@@ -60,12 +59,10 @@ class Cube {
             let t56 = i/(div+1);
             let t51 = j/(div+1);  
             let point = new Vec4(v51.x*t51 + v56.x*t56 + p5.x, v51.y*t51 + v56.y*t56 + p5.y, v51.z*t51 + v56.z*t56 + p5.z, 1);
-            let center_to_point = new Vec4(point.x-center.x, point.y-center.y, point.z-center.z, 1);
-            let normalized_vec = center_to_point.normalizedVec();
-            this.LEFTPoints.push(new Vec4(normalized_vec.x+center.x, normalized_vec.y+center.y, normalized_vec.z+center.z, 1));
-            //this.LEFTPoints.push(point);
+            this.LEFTPoints.push(point);
         }
         }
+        //this.ToSphere(this.LEFTPoints);
 
         for (let i = 0; i < div+1; i++){
         for (let j = 0; j < div+1; j++){
@@ -75,6 +72,7 @@ class Cube {
         }
         }
 
+        //
         //BACK (P5,P6,P7,P8)
         this.BACKPoints = [];
         this.BACKTris = [];
@@ -85,12 +83,10 @@ class Cube {
             let t78 = i/(div+1);
             let t75 = j/(div+1);  
             let point = new Vec4(v78.x*t78 + v75.x*t75 + p7.x, v78.y*t78 + v75.y*t75 + p7.y, v78.z*t78 + v75.z*t75 + p7.z, 1);
-            let center_to_point = new Vec4(point.x-center.x, point.y-center.y, point.z-center.z, 1);
-            let normalized_vec = center_to_point.normalizedVec();
-            this.BACKPoints.push(new Vec4(normalized_vec.x+center.x, normalized_vec.y+center.y, normalized_vec.z+center.z, 1));
-            //this.BACKPoints.push(point);
+            this.BACKPoints.push(point);
         }
         }
+        //this.ToSphere(this.BACKPoints);
 
         for (let i = 0; i < div+1; i++){
         for (let j = 0; j < div+1; j++){
@@ -100,6 +96,7 @@ class Cube {
         }
         }
 
+        //
         //RIGHT (P3,P4,P7,P8)
         this.RIGHTPoints = [];
         this.RIGHTTris = [];
@@ -110,12 +107,10 @@ class Cube {
             let t34 = i/(div+1);
             let t37 = j/(div+1);  
             let point = new Vec4(v34.x*t34 + v37.x*t37 + p3.x, v34.y*t34 + v37.y*t37 + p3.y, v34.z*t34 + v37.z*t37 + p3.z, 1);
-            let center_to_point = new Vec4(point.x-center.x, point.y-center.y, point.z-center.z, 1);
-            let normalized_vec = center_to_point.normalizedVec();
-            this.RIGHTPoints.push(new Vec4(normalized_vec.x+center.x, normalized_vec.y+center.y, normalized_vec.z+center.z, 1));
-            //this.RIGHTPoints.push(point);
+            this.RIGHTPoints.push(point);
         }
         }
+        //this.ToSphere(this.RIGHTPoints);
 
         for (let i = 0; i < div+1; i++){
         for (let j = 0; j < div+1; j++){
@@ -125,6 +120,7 @@ class Cube {
         }
         }
 
+        //
         //TOP (P1,P3,P5,P7)
         this.TOPPoints = [];
         this.TOPTris = [];
@@ -135,12 +131,10 @@ class Cube {
             let t51 = i/(div+1);
             let t57 = j/(div+1);  
             let point = new Vec4(v51.x*t51 + v57.x*t57 + p5.x, v51.y*t51 + v57.y*t57 + p5.y, v51.z*t51 + v57.z*t57 + p5.z, 1);
-            let center_to_point = new Vec4(point.x-center.x, point.y-center.y, point.z-center.z, 1);
-            let normalized_vec = center_to_point.normalizedVec();
-            this.TOPPoints.push(new Vec4(normalized_vec.x+center.x, normalized_vec.y+center.y, normalized_vec.z+center.z, 1));
-            //this.TOPPoints.push(point);
+            this.TOPPoints.push(point);  
         }
         }
+        //this.ToSphere(this.TOPPoints);
 
         for (let i = 0; i < div+1; i++){
         for (let j = 0; j < div+1; j++){
@@ -150,6 +144,7 @@ class Cube {
         }
         }
 
+        //
         //BOTTOM (P2,P4,P6,P8)
         this.BOTTOMPoints = [];
         this.BOTTOMTris = [];
@@ -160,12 +155,11 @@ class Cube {
             let t26 = i/(div+1);
             let t24 = j/(div+1);  
             let point = new Vec4(v26.x*t26 + v24.x*t24 + p2.x, v26.y*t26 + v24.y*t24 + p2.y, v26.z*t26 + v24.z*t24 + p2.z, 1);
-            let center_to_point = new Vec4(point.x-center.x, point.y-center.y, point.z-center.z, 1);
-            let normalized_vec = center_to_point.normalizedVec();
-            this.BOTTOMPoints.push(new Vec4(normalized_vec.x+center.x, normalized_vec.y+center.y, normalized_vec.z+center.z, 1));
-            //this.BOTTOMPoints.push(point);
+            this.BOTTOMPoints.push(point);
+            
         }
         }
+        //this.ToSphere(this.BOTTOMPoints);
 
         for (let i = 0; i < div+1; i++){
         for (let j = 0; j < div+1; j++){
@@ -185,6 +179,14 @@ class Cube {
             this.RIGHTTris[tri].drawTriangle();
             this.TOPTris[tri].drawTriangle();
             this.BOTTOMTris[tri].drawTriangle();
+        }
+    }
+
+    ToSphere(PointList){
+        for (let point of PointList){
+            let center_to_point = new Vec4(point.x-this.centerPoint.x, point.y-this.centerPoint.y, point.z-this.centerPoint.z, 1);
+            let normalized_vec = center_to_point.normalizedVec();
+            PointList[PointList.indexOf(point)] = normalized_vec;
         }
     }
 }
